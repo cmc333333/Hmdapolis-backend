c96b95164a97bfff7d6c0d2f9068acd90cf04947
CM Lubinski <cm.lubinski@gmail.com>
2013-02-10 22:15:14 +0100

    Add cities

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 from flask import abort, Flask, Response, request
 import json
+import psycopg2
 
 app = Flask(__name__)
 
@@ -27,6 +28,21 @@ agencies = [
     ('CFPB', 'Consumer Financial Protection Bureau')
     ]
 
+cities = {
+    31084: 'Los Angeles',  16974: 'Chicago',      19124: 'Dallas', 
+    35644: 'NYC',          44060: 'Spokane',      12580: 'Baltimore',
+    12420: 'Austin',       35380: 'New Orleans',  24340: 'Grand Rapids',
+    41884: 'San Francisco',14484: 'Boston',       42644: 'Seattle',
+    12060: 'Atlanta',      26900: 'Indianapolis', 33460: 'St. Paul',
+    19780: 'Des Moines',   39300: 'Providence',   40900: 'Sacramento',
+    45220: 'Tallahassee',  22020: 'Fargo',        41180: 'St. Louis',
+    29620: 'Lansing',      34980: 'Nashville',    39580: 'Raleigh',
+    38900: 'Portland',     30460: 'Lexington',    43620: 'Sioux Falls',
+    39900: 'Reno',         29820: 'Las Vegas',    17900: 'Columbia',
+    47894: 'D.C',          13820: 'Birmingham',   26420: 'Houston',
+    11260: 'Anchorage',    26180: 'Honolulu',     19740: 'Denver'
+    }
+
 
 @app.route("/")
 def hello():
@@ -34,14 +50,25 @@ def hello():
         {'name': 'accepted', 'count': 8},
         {'name': 'rejected', 'count': 4},
         ]}
-    if request.args.get("callback"):
-        callback = request.args.get("callback")
-        return callback + '(' + json.dumps(data) + ');'
-    return json.dumps(data)
+    return jsonp(data)
 
-@app.route("/dev/")
-def dev():
-    import psycopg2
+def jsonp(to_return):
+    if request.args.get("callback"):
+        return request.args.get("callback") + \
+                '(' + json.dumps(to_return) + ')'
+    return json.dumps(to_return)
+
+@app.route("/agency/")
+def agencies_handler():
+    return jsonp([{'id': agency[0], 'name': agency[1]} for agency 
+        in agencies if agency])
+
+@app.route("/city/")
+def cities_handler():
+    return jsonp(cities)
+
+@app.route("/apply/")
+def apply():
     conn = psycopg2.connect("dbname=hmda user=hmda password=hmda")
     cursor = conn.cursor()
 
@@ -56,7 +83,11 @@ def dev():
         year = int(request.args.get('year'))
         loan_amount = int(request.args.get('loan_amount'))
         msa_md = int(request.args.get('msa_md'))
-        agency = int(request.args.get('agency'))
+        agency = request.args.get('agency')
+        if agency not in [a[0] for a in agencies if a]:
+            abort(400)
+        agency = (idx for idx,a in enumerate(agencies) 
+                if a and a[0] == agency).next()
         if agency == 4 or agency == 9:  #   OTS -> CFPB
             agency_pair = (4,9)
         else:
@@ -85,11 +116,7 @@ def dev():
     for row in cursor.fetchall():
         results[actions[row[1]][1]] += row[0]
 
-    if request.args.get("callback"):
-        return request.args.get("callback") + \
-            '(' + json.dumps(results) + ');'
-
-    return json.dumps(results)
+    return jsonp(results)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8180, debug=True)
